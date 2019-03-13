8e4f1fb73338df03922ff15b9f895c8e10f79515
Rodean Fraser <47337725+deanovision@users.noreply.github.com>
2019-03-13 14:28:24 -0400

    Footer (#8)
    * initial commit, setup html css and js
    
    * started work on top navigation
    
    * added img and background to header
    
    * updated header image and added cta text and button
    
    * updated header font sizing and cta button sizing
    
    * added content to section1
    
    * added more placeholder content to the main section
    
    * added place holder content and styling to the mid section of the site
    
    * finished adding placeholder images and content to section 2
    
    * added placeholder content and styling to section 3
    
    * added a footer and finished tab navigator in section 3
    
    * finished adding navigation and cta button to footer, created second webpage to showcase team members
    
    * added more content to second page
    
    * adjusted width to be responsive on second webpage

--- js/index.js
+++ js/index.js
@@ -1,1 +1,47 @@
-console.log("connected");
+class Tab{
+    constructor(tabElement){
+        this.tabElement = tabElement;
+        console.log(tabElement);
+        this.tabId = this.tabElement.dataset.tab;
+        this.tabContent = document.querySelector(`.features-content[data-tab="${this.tabId}"]`);
+        //console.log(this.tabId);
+       /* 
+        this.content = document.querySelectorAll(".features-content");
+        this.content.forEach(function(contentInfo){
+            return new Content(contentInfo);
+        });*/
+        this.featureContent = new Content(this.tabContent);
+        this.tabElement.addEventListener("click", () => this.selected());      
+    }
+    selected(){
+        console.log("clicked");
+       const allTabs = document.querySelectorAll(".tab-button");
+        allTabs.forEach(function(tabInfo){
+            tabInfo.classList.remove("selected");
+        });
+        this.tabElement.classList.add("selected");
+        
+        this.featureContent.contentSelected();
+        
+    }
+}
+
+class Content {
+    constructor(contentInfo){
+        this.content = contentInfo;
+        console.log(this.content);
+        this.contentId = this.content.dataset.tab;
+    }
+    contentSelected(){
+        const selected = document.querySelectorAll(".features-content");
+        selected.forEach(function(content){
+            content.classList.add("hide-content");
+        });
+        this.content.classList.remove("hide-content");
+    }
+}
+
+const tabs = document.querySelectorAll(".tab-button");
+tabs.forEach(function(tabInfo){
+    return new Tab(tabInfo);
+})
