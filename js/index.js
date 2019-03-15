6f5ea706538cc9304f2e0b579dc9decb990717d2
deanovision <rodeanfraser@gmail.com>
2019-03-15 10:57:33 -0400

    finished toggle menu for mobile

--- js/index.js
+++ js/index.js
@@ -1,35 +1,24 @@
 class Tab{
     constructor(tabElement){
         this.tabElement = tabElement;
-        console.log(tabElement);
         this.tabId = this.tabElement.dataset.tab;
         this.tabContent = document.querySelector(`.features-content[data-tab="${this.tabId}"]`);
-        //console.log(this.tabId);
-       /* 
-        this.content = document.querySelectorAll(".features-content");
-        this.content.forEach(function(contentInfo){
-            return new Content(contentInfo);
-        });*/
         this.featureContent = new Content(this.tabContent);
         this.tabElement.addEventListener("click", () => this.selected());      
     }
     selected(){
-        console.log("clicked");
        const allTabs = document.querySelectorAll(".tab-button");
         allTabs.forEach(function(tabInfo){
             tabInfo.classList.remove("selected");
         });
         this.tabElement.classList.add("selected");
         
-        this.featureContent.contentSelected();
-        
+        this.featureContent.contentSelected();        
     }
 }
-
 class Content {
     constructor(contentInfo){
         this.content = contentInfo;
-        console.log(this.content);
         this.contentId = this.content.dataset.tab;
     }
     contentSelected(){
@@ -40,21 +29,17 @@ class Content {
         this.content.classList.remove("hide-content");
     }
 }
-
 const tabs = document.querySelectorAll(".tab-button");
 tabs.forEach(function(tabInfo){
     return new Tab(tabInfo);
 })
-
 const menu = document.querySelector(".nav-bar-mobile");
 const menuButton = document.querySelector(".menu-button")
 menuButton.addEventListener("click", function(){
     return menu.classList.toggle("hide-content");
 });
-
-const links = document.querySelectorAll(".nav-bar-mobile div");
-links.forEach(function(link){
-    link.addEventListener("click", function(){
-        menu.classList.toggle("hide-content")
-    });
-});
+window.addEventListener("click",function(event){
+    if(event.target !== menuButton){
+        menu.classList.add("hide-content");
+    }
+}); 
